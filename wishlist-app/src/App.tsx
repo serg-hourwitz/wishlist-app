3ed50c3114d50ece1355db6264158cf134be3126
Serhiy Hourwitz <shmone800@gmail.com>
2025-12-23 14:49:52 +0200

    chore: init React + TypeScript project

---
+++ wishlist-app/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-2xl font-bold text-red-900">
+      Tailwind працює ✅
+    </div>
+  );
+}
+
+export default App
