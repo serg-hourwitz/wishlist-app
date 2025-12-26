2e9931381cd88e4f3e656387e53f9ccd89bb5543
Serhiy Hourwitz <shmone800@gmail.com>
2025-12-26 15:44:57 +0200

    added basename to router

--- wishlist-app/src/App.tsx
+++ wishlist-app/src/App.tsx
@@ -6,7 +6,7 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/wishlist-app">
       <WishesProvider>
         <Routes>
           <Route path="/" element={<DashboardPage />} />
